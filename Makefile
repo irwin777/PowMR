738067f6d4680b9a78febde5bd3ccb13e204a243
irwin777 <irwin.7777@gmail.com>
2023-03-31 10:09:10 +0300

    rrd

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 build:
-	git pull
-	go build -o bin/test main.go
+	GOARCH=arm GOARM=7 go build -o bin/ipwm3000 main.go
 install:build
-	systemctl stop ipwm3000
-	rm test
-	cp bin/test .
-	systemctl start ipwm3000
-	systemctl status ipwm3000
+	ssh root@172.20.20.15 "systemctl stop ipwm3000"
+	scp bin/ipwm3000 root@172.20.20.15:/root/PowMR/
+	ssh root@172.20.20.15 "systemctl start ipwm3000"
+	ssh root@172.20.20.15 "systemctl status ipwm3000"
--- main.go
+++ main.go
@@ -3,40 +3,31 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/snksoft/crc"
 	"github.com/tarm/serial"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
-type status struct {
-	gorm.Model
-	DeviceMode              string
-	ACvoltage               float64
-	ACfrequency             float64
-	OutputVoltage           float64
-	OutputFrequency         float64
-	OutputActivePower       int
-	OutputApparntPower      int
-	OutputPower             int
-	ChargingCurrent         int
-	PVinputCurrent          int
-	PVinputVoltage          float64
-	BatteryVoltage          float64
-	BatteryDischargeCurrent int
-	BatteryCapacity         int
-}
+const (
+	ACv string = "db/ACv.rrd" //AC Voltage IN OUT
+	ACf string = "db/ACf.rrd" //AC Freq IN OUT
+	OPa string = "db/OPa.rrd" //Outpet Power Active Apparnt
+	OPp string = "db/OPp.rrd" //Output Power %
+	PV  string = "db/PV.rrd"  //PV Volt Amper IN
+	BC  string = "db/BC.rrd"  //Battery chatging discharging
+	Bv  string = "db/Bv.rrd"  //Battery Voltage
+	Bc  string = "db/Bc.rrd"  //Battery capacity %
+)
 
 var (
-	Db *gorm.DB
+	Mode string
 )
 
 func init() {
@@ -63,13 +54,130 @@ func init() {
 			log.Fatalln(err)
 		}
 	}
-	Db, err = gorm.Open(sqlite.Open("db/solar.db"), &gorm.Config{})
+
+	_, err = os.Stat(ACv)
 	if err != nil {
-		log.Fatalln("failed to open database")
+		cmd := exec.Command("rrdtool", "create", ACv, "--step 60",
+			"DS:Vi:GAUGE:120:0:250",
+			"DS:Vo:GAUGE:120:0:250",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
-	err = Db.AutoMigrate(&status{})
+
+	_, err = os.Stat(ACf)
 	if err != nil {
-		log.Println(err)
+		cmd := exec.Command("rrdtool", "create", ACf, "--step 60",
+			"DS:Fi:GAUGE:120:0:60",
+			"DS:Fo:GAUGE:120:0:60",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(OPa)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", OPa, "--step 60",
+			"DS:Ac:GAUGE:120:0:U",
+			"DS:Ap:GAUGE:120:0:U",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(OPp)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", OPp, "--step 60",
+			"DS:Op:GAUGE:120:0:100",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(PV)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", PV, "--step 60",
+			"DS:V:GAUGE:120:0:50",
+			"DS:A:GAUGE:120:0:25",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(BC)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", BC, "--step 60",
+			"DS:Bc:GAUGE:120:0:U",
+			"DS:Bd:GAUGE:120:0:U",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(Bv)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", Bv, "--step 60",
+			"DS:Bv:GAUGE:120:0:36",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	_, err = os.Stat(Bc)
+	if err != nil {
+		cmd := exec.Command("rrdtool", "create", Bc, "--step 60",
+			"DS:Bc:GAUGE:120:0:100",
+			"RRA:AVERAGE:0.5:1:15",
+			"RRA:AVERAGE:0.5:10:144",
+			"RRA:AVERAGE:0.5:60:168",
+			"RRA:AVERAGE:0.5:360:87600",
+			"RRA:AVERAGE:0.5:720:87600")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 }
 
@@ -122,7 +230,6 @@ func getData(rq string) (string, error) {
 }
 
 func getStatus() {
-
 	/*
 	   'QPI'		=> "515049beac0d", ##  Device Protocol ID Inquiry
 	   	'QID'		=> "514944d6ea0d", ## Device Serial Number Inquiry
@@ -146,13 +253,13 @@ func getStatus() {
 	   	'QBEGI'		=> "51424551492ea90d", ## nicht dokumentierti
 	   	'QMOD'		=> "514d4f4449c10d" ## Device Mode inquiry
 	*/
-	var lastStatus status
+
 	rcv, err := getData("QMOD")
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	lastStatus.DeviceMode = rcv
+	Mode = rcv
 
 	rcv, err = getData("QPIGS")
 	if err != nil {
@@ -160,36 +267,86 @@ func getStatus() {
 		return
 	}
 	ss := strings.Split(rcv, " ")
+
 	ACvoltage := ss[0]
-	lastStatus.ACvoltage, _ = strconv.ParseFloat(ACvoltage, 32)
-	ACfrequency := ss[1]
-	lastStatus.ACfrequency, _ = strconv.ParseFloat(ACfrequency, 32)
+	acvi, _ := strconv.ParseFloat(ACvoltage, 32)
 	Outputvoltage := ss[2]
-	lastStatus.OutputVoltage, _ = strconv.ParseFloat(Outputvoltage, 32)
-	Outputfrequency := ss[3]
-	lastStatus.OutputFrequency, _ = strconv.ParseFloat(Outputfrequency, 32)
-	OutputActivePower := ss[4]
-	lastStatus.OutputActivePower, _ = strconv.Atoi(OutputActivePower)
-	OutputApparntPower := ss[5]
-	lastStatus.OutputApparntPower, _ = strconv.Atoi(OutputApparntPower)
-	OutputPower := ss[6]
-	lastStatus.OutputPower, _ = strconv.Atoi(OutputPower)
-	ChargingCurrent := ss[9]
-	lastStatus.ChargingCurrent, _ = strconv.Atoi(ChargingCurrent)
-	PVInputCurrent := ss[12]
-	lastStatus.PVinputCurrent, _ = strconv.Atoi(PVInputCurrent)
-	PVInputVoltage := ss[13]
-	lastStatus.PVinputVoltage, _ = strconv.ParseFloat(PVInputVoltage, 32)
-	BatteryVoltage := ss[8]
-	lastStatus.BatteryVoltage, _ = strconv.ParseFloat(BatteryVoltage, 32)
-	BatteryDischargeCurrent := ss[15]
-	lastStatus.BatteryDischargeCurrent, _ = strconv.Atoi(BatteryDischargeCurrent)
-	BatteryCapacity := ss[17]
-	lastStatus.BatteryCapacity, _ = strconv.Atoi(BatteryCapacity)
-	tx := Db.Create(&lastStatus)
-	if tx.Error != nil {
-		log.Println(tx.Error)
+	acvo, _ := strconv.ParseFloat(Outputvoltage, 32)
+	acvupd := fmt.Sprintf("N:%.2f:%.2f", acvi, acvo)
+	acv := exec.Command("rrdtool", "update", ACv, acvupd)
+	err = acv.Run()
+	if err != nil {
+		log.Println(err)
 	}
-	jb, _ := json.MarshalIndent(lastStatus, "", " ")
-	fmt.Print(string(jb))
+
+	ACfrequency := ss[1]
+	acfi, _ := strconv.ParseFloat(ACfrequency, 32)
+	Outputfrequency := ss[3]
+	acfo, _ := strconv.ParseFloat(Outputfrequency, 32)
+	acfupd := fmt.Sprintf("N:%.2f:%.2f", acfi, acfo)
+	acf := exec.Command("rrdtool", "update", ACf, acfupd)
+	err = acf.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	OutputActivePower := ss[4]
+	oac, _ := strconv.Atoi(OutputActivePower)
+	OutputApparntPower := ss[5]
+	oap, _ := strconv.Atoi(OutputApparntPower)
+	oapupd := fmt.Sprintf("N:%d:%d", oac, oap)
+	oapu := exec.Command("rrdtool", "update", OPa, oapupd)
+	err = oapu.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	OutputPower := ss[6]
+	oapp, _ := strconv.Atoi(OutputPower)
+	oappupd := fmt.Sprintf("N:%d", oapp)
+	oappu := exec.Command("rrdtool", "update", OPp, oappupd)
+	err = oappu.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	PVInputCurrent := ss[12]
+	pva, _ := strconv.Atoi(PVInputCurrent)
+	PVInputVoltage := ss[13]
+	pvv, _ := strconv.ParseFloat(PVInputVoltage, 32)
+	pvu := fmt.Sprintf("N:%.2f:%d", pvv, pva)
+	pvupd := exec.Command("rrdtool", "update", PV, pvu)
+	err = pvupd.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	ChargingCurrent := ss[9]
+	bcc, _ := strconv.Atoi(ChargingCurrent)
+	BatteryDischargeCurrent := ss[15]
+	bdc, _ := strconv.Atoi(BatteryDischargeCurrent)
+	bcdu := fmt.Sprintf("N:%d:%d", bcc, bdc)
+	bcdup := exec.Command("rrdtool", "update", BC, bcdu)
+	err = bcdup.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	BatteryVoltage := ss[8]
+	bv, _ := strconv.ParseFloat(BatteryVoltage, 32)
+	bvu := fmt.Sprintf("N:%.2f", bv)
+	bvup := exec.Command("rrdtool", "update", Bv, bvu)
+	err = bvup.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	BatteryCapacity := ss[17]
+	bc, _ := strconv.Atoi(BatteryCapacity)
+	bcu := fmt.Sprintf("N:%d", bc)
+	bcup := exec.Command("rrdtool", "update", Bc, bcu)
+	err = bcup.Run()
+	if err != nil {
+		log.Println(err)
+	}
 }
